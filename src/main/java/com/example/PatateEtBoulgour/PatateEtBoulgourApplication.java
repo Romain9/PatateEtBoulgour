5b0922927d972141013467ab60161ce4d395131b
Romain9 <romain.msiah@gmail.com>
2024-04-23 21:18:25 +0200

    init

---
+++ src/main/java/com/example/PatateEtBoulgour/PatateEtBoulgourApplication.java
@@ -0,0 +1,13 @@
+package com.example.PatateEtBoulgour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatateEtBoulgourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatateEtBoulgourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PatateEtBoulgour/PatateEtBoulgourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PatateEtBoulgour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatateEtBoulgourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
