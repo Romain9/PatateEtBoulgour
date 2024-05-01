9b76ce36fccfc683f87e6dc32576411746bddbae
Gabriel <60087391+g4bey@users.noreply.github.com>
2024-05-01 23:12:48 +0200

    merge

--- src/main/java/com/example/PatateEtBoulgour/controller/HomeController.java
+++ src/main/java/com/example/PatateEtBoulgour/controller/HomeController.java
@@ -34,81 +34,29 @@ public class HomeController {
     private ActivityService activityService;
 
     @GetMapping()
-    public ModelAndView home() {
-        Map<String, Object> m = new HashMap<>();
-
-        Pageable page = PageRequest.of(0, 10);
-        List<Activity> activities = activityService.getAllActivities(page);
-
-        User user = userService.getCurrentUser();
-        if (user != null) {
-            m.put("user", user);
-            for (Activity activity : activities) {
-                if (user.getActivities().contains(activity)) {
-                    activity.setContainsCurrentUser(true);
-                }
-            }
-        } else {
-            m.put("unlogged", true);
-        }
-
-        m.put("nbPage", 0);
-
-        if (!activityService.getAllActivities(page.next()).isEmpty()) {
-            m.put("nextPage", 1);
-        }
-        
-        
-        m.put("activities", activities);
-
-        return new ModelAndView("index", m);
+    public String home(Model m) {
+        m = search(m, "", 0);
+        return "index";
     }
 
-    @PostMapping()
-    public String homeSearch(@RequestParam("search") String activityKeywords, @RequestParam("page") int numPage, Model m) {
+    private Model search(Model m, String activityKeywords, int numPage) {
 
         Pageable page = PageRequest.of(0, 10*(numPage+1));
 
         Set<Activity> activities = activityService.getActivityContainingKeyword(activityKeywords, page);
+
         m.addAttribute("activities", activities);
 
         m.addAttribute("user", userService.getCurrentUser());
 
         m.addAttribute("nbPage", numPage+1);
 
-        return "components/activities";
+        return m;
     }
-    
-    @RequestMapping("/{id}")
-    public ModelAndView getActivityForPage(@PathVariable int id) {
-        Map<String, Object> m = new HashMap<>();
 
-        if (id < 0) {
-            m.put("errorMessage", "Eh oh, tu arrêtes!");
-            return new ModelAndView("redirect:/");
-        }
-
-        Pageable page = PageRequest.of(id, 10);
-        List<Activity> activities = activityService.getAllActivities(page);
-
-        User user = userService.getCurrentUser();
-        if (user != null) {
-            m.put("user", user);
-            for (Activity activity : activities) {
-                if (user.getActivities().contains(activity)) {
-                    activity.setContainsCurrentUser(true);
-                }
-            }
-        } else {
-            m.put("unlogged", true);
-        }
-
-        m.put("activities", activities);
-
-        if (!activityService.getAllActivities(page.next()).isEmpty()) {
-            m.put("nextPage", page.next().getPageNumber());
-        }
-
-        return new ModelAndView("index", m);
+    @PostMapping()
+    public String homeSearch(@RequestParam("search") String activityKeywords, @RequestParam("page") int numPage, Model m) {
+        m = search(m, activityKeywords, numPage);
+        return "components/activities";
     }
 }
